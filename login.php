b95daf99ac065e6fb0e4a9ad9fdd642eeafb5cc0
Kadri Alumets <kadralu@greeny.cs.tlu.ee>
2015-09-09 12:40:36 +0300

    New

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+	
+
+?>
+
+<html>
+<head>
+	<title>Login page></title>
+</head>
+<body>
+	<h2>Log in</h2>
+		<input type="email" placeholder="E-post"> <br><br>
+		<input type="password" placeholder="Parool"> <br><br>
+		<input type="sumbit" value="Log in"> 
+	
+	<h2>Create user</h2>
+</body>
+</html>
